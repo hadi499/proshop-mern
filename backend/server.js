f211ec32bd0ff86ae5c7a43d8104b7a00f72af8f
hadi499 <hadi.ygcl@gmail.com>
2023-01-19 13:37:02 +0700

    require modul

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const products = require("./data/products");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.get("/", (req, res) => {
@@ -14,4 +16,5 @@ app.get("/api/product/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
-app.listen(5000, () => console.log("server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
